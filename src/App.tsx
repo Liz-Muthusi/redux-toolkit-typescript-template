4a8cca042ee5eedccbbe5fb9b65d75c816d601de
Elizabeth <mwendemuthusi6@gmail.com>
2023-10-23 09:00:21 +0300

    starter redux typescript template

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
-import CakeView from "./app/features/cake/CakeView";
-import CreamView from "./app/features/icecream/CreamView";
+
 import UserView from "./app/features/users/UserView";
 
 function App() {
   return (
     <>
-      <CakeView />
-      <CreamView />
       <UserView />
     </>
   );
